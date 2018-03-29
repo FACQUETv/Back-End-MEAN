0dc79aa30cfd7af19419c969df9b1536eacb3c34
FACQUET <valentin.facquet@capgemini.com>
2018-03-29 10:21:44 +0200

    first commit

---
+++ controllers/todos.controller.js
@@ -0,0 +1,78 @@
+var TodoService = require('../services/todos.service')
+
+_this = this
+
+// HTTP GET
+exports.getTodos = async function(req, res, next){
+
+    var page = req.query.page ? req.query.page : 1
+    var limit = req.query.limit ? req.query.limit : 10; 
+
+    console.log(page, limit)
+
+    try{
+        var todos = await TodoService.getTodos({}, page, limit)
+        return res.status(200).json({status: 200, data: todos, message: "Succesfully Todos Recieved"});
+    }catch(e){
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
+
+// HTTP POST
+exports.createTodo = async function(req, res, next){
+    var todo = {
+        address : req.body.address,
+        borough : req.body.borough,
+        cuisine : req.body.status,
+        grades : req.body.score,
+        name : req.body.name,
+        restaurant_id : req.body.restaurant_id
+    }
+
+    try{
+        var createdTodo = await TodoService.createTodo(todo)
+        return res.status(201).json({status: 201, data: createdTodo, message: "Succesfully Created ToDo"})
+    }catch(e){
+        return res.status(400).json({status: 400, message: "Todo Creation was Unsuccesfull"})
+    }
+}
+
+// HTTP PUT
+exports.updateTodo = async function(req, res, next){
+
+    if(!req.body._id){
+        return res.status(400).json({status: 400., message: "Id must be present"})
+    }
+
+    var id = req.body._id;
+
+    console.log(req.body)
+
+    var todo = {
+        id,
+        title: req.body.title ? req.body.title : null,
+        description: req.body.description ? req.body.description : null,
+        status: req.body.status ? req.body.status : null
+    }
+
+    try{
+        var updatedTodo = await TodoService.updateTodo(todo)
+        return res.status(200).json({status: 200, data: updatedTodo, message: "Succesfully Updated Tod"})
+    }catch(e){
+        return res.status(400).json({status: 400., message: e.message})
+    }
+}
+
+// HTTP DELETE
+exports.removeTodo = async function(req, res, next){
+
+    var id = req.params.id;
+
+    try{
+        var deleted = await TodoService.deleteTodo(id)
+        return res.status(204).json({status:204, message: "Succesfully Todo Deleted"})
+    }catch(e){
+        return res.status(400).json({status: 400, message: e.message})
+    }
+
+}
---
+++ models/todo.model.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+var mongoosePaginate = require('mongoose-paginate')
+
+
+var ToDoSchema = new mongoose.Schema({
+    address: {building : String, coord : Object, street: String, zipcode : String},
+    borough: String,
+    cuisine: String,
+    grades: {date: {$date: String, grade: String, score: String}},
+    name : String,
+    restaurant_id : String
+})
+
+ToDoSchema.plugin(mongoosePaginate)
+const ToDo = mongoose.model('restaurant', ToDoSchema)
+
+module.exports = ToDo;
